323f2907bc7ab6e23676b918f7b6ac3aedc6fa20
dirkf <fieldhouse@gmx.net>
2023-07-25 00:17:15 +0100

    [compat] Use `compat_open()`

--- devscripts/make_readme.py
+++ devscripts/make_readme.py
@@ -10,6 +10,8 @@ sys.path.insert(0, dirn(dirn(os.path.abspath(__file__))))
 from utils import read_file
 from youtube_dl.compat import compat_open as open
 
+from youtube_dl.compat import compat_open as open
+
 README_FILE = 'README.md'
 helptext = sys.stdin.read()
 
